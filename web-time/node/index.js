17fdb5c95d807392f330c53dabdef3c5a2680202
rahulroy9202 <rahulroy9202@gmail.com>
2015-03-09 14:10:31 +0530

    now server tick more synchronized with server time

---
+++ web-time/node/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.redirect('/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+  
+  setTimeout(function(){
+	setInterval(timeBroadcast,1000);
+  },(1000 - (new Date()).getMilliseconds()));
+  
+});
+
+function timeBroadcast(){
+  io.emit('time_broadcast', new Date());
+  console.log((new Date()).getMilliseconds());
+}
