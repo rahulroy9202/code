13a4b6aa4933a51243e371e1d08ffd9fbe222515
rahulroy9202 <rahulroy9202@gmail.com>
2015-03-09 14:16:42 +0530

    server time tick and socket emit synchronized. offest < 5

--- web-time/node/index.js
+++ web-time/node/index.js
@@ -21,12 +21,16 @@ http.listen(8080, function(){
   console.log('listening on *:8080');
   
   setTimeout(function(){
-	setInterval(timeBroadcast,1000);
+	timeBroadcast();
   },(1000 - (new Date()).getMilliseconds()));
   
 });
 
 function timeBroadcast(){
   io.emit('time_broadcast', new Date());
-  console.log((new Date()).getMilliseconds());
+  //console.log((new Date()).getMilliseconds());		//uncomment to get rough offset. should be less than 5
+  
+  setTimeout(function(){
+	timeBroadcast();
+  },(1000 - (new Date()).getMilliseconds()));
 }
