52dd69f22462bb5b2d3fca908157f3c9750ca1f5
rahulroy9202 <rahulroy9202@gmail.com>
2016-05-11 10:58:24 +0530

    working dbstats

--- just-stuff/db-stuff/dbstats.js
+++ just-stuff/db-stuff/dbstats.js
@@ -2,12 +2,24 @@ use fundraising
 
 // Total campaigns 
 print('Total campaigns in LFC 16')
-
 db.campaigns.find({created_at: {
         $gte: ISODate("2016-03-07T18:30:00.000Z"),
         $lt: ISODate("2016-04-28T18:29:00.000Z")
     }}).count()
 	
 print('Total campaigns in Product')
-use fundraising
 db.campaigns.count()
+
+// Total invites
+print('Total invites in LFC 16')
+db.inviteemails.find({created: {
+        $gte: ISODate("2016-03-07T18:30:00.000Z"),
+        $lt: ISODate("2016-04-28T18:29:00.000Z")
+    }}).count()
+	
+print('Total invites in Product')
+db.inviteemails.count()
+
+// Total number of donations
+print('Total number of donations')
+db.donations.find({"status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']}}).count()
