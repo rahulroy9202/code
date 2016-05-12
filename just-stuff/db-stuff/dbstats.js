97e8108b268118f274332b9aa9d13ea22f972dec
rahulroy9202 <rahulroy9202@gmail.com>
2016-05-12 10:57:39 +0530

    No. of Fundraisers (since launch)

--- just-stuff/db-stuff/dbstats.js
+++ just-stuff/db-stuff/dbstats.js
@@ -1,39 +1,47 @@
-use fundraising
+use fundraising;
 
 // Campaigns 
-print('Total campaigns in LFC 16')
+print('Total campaigns in LFC 16');
 db.campaigns.find({created_at: {
         $gte: ISODate("2016-03-07T18:30:00.000Z"),
         $lt: ISODate("2016-04-28T18:29:00.000Z")
-    }}).count()
+    }}).count();
 	
-print('Total campaigns in Product')
-db.campaigns.count()
+print('Total campaigns in Product');
+db.campaigns.count();
 
 // Invites
-print('Total invites in LFC 16')
+print('Total invites in LFC 16');
 db.inviteemails.find({created: {
         $gte: ISODate("2016-03-07T18:30:00.000Z"),
         $lt: ISODate("2016-04-28T18:29:00.000Z")
-    }}).count()
+    }}).count();
 	
-print('Total invites in Product')
-db.inviteemails.count()
+print('Total invites in Product');
+db.inviteemails.count();
 
 // Total number of donations
-print('Total number of donations')
-db.donations.find({"status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']}}).count()
+print('Total number of donations');
+db.donations.find({"status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']}}).count();
 
 // Total donation amount 
-print('Total donation amount')
+print('Total donation amount');
 db.donations.aggregate([
 	{ $match: { "status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']} } },
 	{ $group: { _id: "$currency_code", "total_donation": { "$sum": "$amount" } } }
-])
+]);
 
 // Average donation amount 
-print('Average donation amount')
+print('Average donation amount');
 db.donations.aggregate([
 	{ $match: { "status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']} } },
 	{ $group: { _id: "$currency_code", "avg_donation": { "$avg": "$amount" } } }
-])
+]);
+
+// No. of Fundraisers (since launch)
+print('No. of Fundraisers (since launch)');
+db.campaigns.aggregate([
+	{ $match: {created_at: {$gte: ISODate("2016-03-07T18:30:00.000Z")}}},
+	{ $group: { _id: "$creator" }},
+	{ $group: { _id: null, count: { $sum: 1 }}}
+]);
