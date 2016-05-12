4f9acfae31a9c77495b6b30671c56af3833376a9
rahulroy9202 <rahulroy9202@gmail.com>
2016-05-12 13:27:41 +0530

    cleanup and formatting and using distinct instead of aggregation when possible

--- just-stuff/db-stuff/dbstats.js
+++ just-stuff/db-stuff/dbstats.js
@@ -1,45 +1,45 @@
 use fundraising;
 
 // Campaigns 
-print('Total campaigns in LFC 16');
+print('\nTotal campaigns in LFC 16');
 db.campaigns.find({created_at: {
         $gte: ISODate("2016-03-07T18:30:00.000Z"),
         $lt: ISODate("2016-04-28T18:29:00.000Z")
     }}).count();
 	
-print('Total campaigns in Product');
+print('\nTotal campaigns in Product');
 db.campaigns.count();
 
 // Invites
-print('Total invites in LFC 16');
+print('\nTotal invites in LFC 16');
 db.inviteemails.find({created: {
         $gte: ISODate("2016-03-07T18:30:00.000Z"),
         $lt: ISODate("2016-04-28T18:29:00.000Z")
     }}).count();
 	
-print('Total invites in Product');
+print('\nTotal invites in Product');
 db.inviteemails.count();
 
 // Total number of donations
-print('Total number of donations');
+print('\nTotal number of donations');
 db.donations.find({"status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']}}).count();
 
 // Total donation amount 
-print('Total donation amount');
+print('\nTotal donation amount');
 db.donations.aggregate([
 	{ $match: { "status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']} } },
 	{ $group: { _id: "$currency_code", "total_donation": { "$sum": "$amount" } } }
 ]);
 
 // Average donation amount 
-print('Average donation amount');
+print('\nAverage donation amount');
 db.donations.aggregate([
 	{ $match: { "status" : {$in : ['CONFIRMED', 'DISBURSED', 'SETTLED']} } },
 	{ $group: { _id: "$currency_code", "avg_donation": { "$avg": "$amount" } } }
 ]);
 
 // No. of Fundraisers (since launch)
-print('No. of Fundraisers (since launch)');
+print('\nNo. of Fundraisers (since launch)');
 db.campaigns.aggregate([
 	{ $match: {created_at: {$gte: ISODate("2016-03-07T18:30:00.000Z")}}},
 	{ $group: { _id: "$creator.id" }},
@@ -48,21 +48,19 @@ db.campaigns.aggregate([
 
 // No. of donors converted to fundraisers and as % of overall donors  
 // % of donors woh are fundraisers
-print('No. of Fundraisers');
-db.campaigns.aggregate([
-	{ $group: { _id: "$creator.id" }},
-	{ $group: { _id: null, count: { $sum: 1 }}}
-]);
+print('\nNo. of Fundraisers');
+var fundraisers = db.campaigns.distinct("creator.id");
+print(fundraisers.length);
+print('\nNo. of Donors');
+var donors = db.donations.distinct("user._id");
+print(donors.length);
+print('\nNo. of donors converted to fundraisers and as % of overall donors');
+print((fundraisers.length/donors.length)*100);
 
-print('No. of Donors');
-db.donations.aggregate([
-	{ $group: { _id: "$user._id" }},
-	{ $group: { _id: null, count: { $sum: 1 }}}
-]);
 
 // % funding targets reached via campaigns 
 // What % of campaigns achieved funding target
-print('% funding targets reached via campaigns');
+print('\n% funding targets reached via campaigns');
 var myCursor =  db.campaigns.find({});
 var fundedCount = 0;
 var totalCount = 0;
